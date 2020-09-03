00a98b17bed635781a73e67f078d771176cd67b7
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-09-03 23:38:16 +0530

    pushed to Github

--- task2.py
+++ task2.py
@@ -1,1 +1,12 @@
-# Task 2 of the QOSF Mentooship programme
+## Task 2 of the QOSF Mentooship programme
+## To implement a circuit which returns |01> and |10> with equal probability
+
+# Note the difference in the ordering of qubits in Qiskit
+
+# Importing necessary libraries
+from qiskit import *
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Define a quantum circuit of 2 qubits and 2 classical bits
+qc = QuantumCircuit(2,2)
