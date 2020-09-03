67cac1ed72e2c816a31e609c2aefce6ce09a2904
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-09-04 00:10:56 +0530

    implemented basic construction of the problem

--- task2.py
+++ task2.py
@@ -5,8 +5,25 @@
 
 # Importing necessary libraries
 from qiskit import *
+from qiskit.visualization import plot_histogram
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Define a quantum circuit of 2 qubits and 2 classical bits
-qc = QuantumCircuit(2,2)
+qc = QuantumCircuit(2)
+#qc.x(0)
+qc.rx(np.pi, 0)
+
+#qc.h(1)
+qc.rx(np.pi/2, 1)
+qc.rz(np.pi/2, 1)
+
+qc.cx(1,0)
+qc.measure_all()
+
+backend = Aer.get_backend('qasm_simulator')
+
+counts = execute(qc, backend).result().get_counts()
+plot_histogram(counts)
+#qc.draw()
+plt.show()
