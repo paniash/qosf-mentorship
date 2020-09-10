9b0434befbfcbcf3362f06f30ce64185797760df
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-09-11 00:18:55 +0530

    finished basic implementation

--- task2.py
+++ task2.py
@@ -1,5 +1,5 @@
 '''
-Task 2 of the QOSF Mentooship programme
+Task 2 of the QOSF Mentorship programme
 To implement a circuit which returns |01> and |10> with equal probability
 '''
 
@@ -20,9 +20,8 @@ qc = QuantumCircuit(2)
 qc.rx(np.pi, 1)
 
 # Hadamard gate
-qc.ry(np.pi/4, 0)
+qc.ry(np.pi/2, 0)
 qc.rx(np.pi, 0)
-qc.ry(-np.pi/4, 0)
 
 qc.cx(0,1)
 qc.measure_all()
@@ -37,39 +36,20 @@ plot_histogram(counts)
 plt.show()
 '''
 
-# Define a quantum circuit of 2 qubits and 2 classical bits
-qc = QuantumCircuit(2, 2)
+# Define a parametric quantum circuit of 2 qubits
+def circuit(params):
+    qc = QuantumCircuit(2)
 
-# Apply an X gate on q_1 and a Hadamard gate on q_0
-# Perform a CNOT with q_0 and q_1 being the control and target qubits respectively
+    # Gates implemented
+    qc.ry(params[0], 1)
+    qc.rx(params[1], 1)
+    qc.ry(params[2], 0)
+    qc.rx(params[3], 0)
 
-# Randomize the parameters of
-theta1 = random.uniform(0, 2*np.pi)
-theta2 = random.uniform(0, 2*np.pi)
-theta3 = random.uniform(0, 2*np.pi)
-theta4 = random.uniform(0, 2*np.pi)
+    return qc
 
-# RX gate on q_1 (to be optimized to X gate)
-qc.rx(theta1, 1)
+# define backends
+q_simulator = Aer.get_backend('qasm_simulator')
+st_simulator = Aer.get_backend('statevector_simulator')
 
-# Gates on q_0 (to be optimized to a Hadamard gate)
-qc.ry(theta2, 0)
-qc.rx(theta3, 0)
-qc.ry(theta4, 0)
-
-# CNOT gate
-qc.cx(0,1)
-
-# Measurement onto classical bits
-qc.measure([0,1], [0,1])
-
-# Backend = QASM simulator
-backend = Aer.get_backend('qasm_simulator')
-
-# Number of measurements
-n = 1000
-
-# Plot the measurement results on a histogram
-counts = execute(qc, backend, shots = n).result().get_counts()
-plot_histogram(counts)
-plt.show()
+def prob(circuit, params, state, qubit)
